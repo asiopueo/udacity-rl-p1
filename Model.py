03b1c0f72ecf5488fbae65eed41ef60e7216bd97
Martin <martin.lippl@pueo.de>
2019-08-09 21:14:33 +0200

    Added DQN-model; still incomplete

---
+++ Model.py
@@ -0,0 +1,42 @@
+
+
+
+
+
+
+
+
+
+
+class Model():
+    def __init__():
+        self.memory = Memory()
+
+        self.short_net =
+        self.long_net =
+
+
+    def learn():
+        pass
+
+    def action():
+        pass
+
+    def update_long():
+        self.long_net = self.short_net
+
+
+
+
+class Memory():
+    __init()__:
+        self.memory = dequeue()
+
+    def insert():
+        pass
+
+    def sample():
+        pass
+
+    def length():
+        pass
