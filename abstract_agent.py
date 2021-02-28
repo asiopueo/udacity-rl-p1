029767f4466c198814dcf53f9fbed53af2a25962
Martin <martin.lippl@pueo.de>
2021-02-28 20:17:01 +0100

    * Created new class AbstractAgent in which the learn()-method is kept abstract * Implementation of the DQL and DDQL algorithms can be found in the children DQLAgent and DDQLAgent in dql_agent.py and ddql_agent.py, respectively

---
+++ abstract_agent.py
@@ -0,0 +1,111 @@
+from collections import deque
+import random
+import os
+import numpy as np
+from collections import namedtuple
+
+import network
+
+from abc import ABC, abstractmethod
+
+#import keras.backend as K
+
+# Available actions are:
+# 1. forward & backward
+# 2. left & right
+
+# State is given by 7 tuples of the form [1,2,3,4,5] and two additional scalars (cf. below):
+# Each of the tuples describes a ray which has been emanated along the angles: [20,90,160,45,135,70,110]
+# Or in ordered sequence: [20,45,70,90,110,135,160]
+# [Yellow Banana, Wall, Blue Banana, Agent, Distance]
+# Ex. [0,0,1,1,0,0.34] means there is
+# The last 2 numbers are the left/right turning velocity v_yaw and the forward/backward velocity v_lat of the agent: [v_yaw, v_lat]
+
+
+class AbstractAgent(ABC):
+    def __init__(self, buffer_size, batch_size, action_size, gamma):
+        if not batch_size < buffer_size:
+            raise Exception()
+
+        self.buffer_size = buffer_size
+        self.batch_size = batch_size
+        self.action_size = 4
+        self.gamma = gamma
+
+        # Initialize replay buffer
+        self.replay_buffer = ReplayBuffer(buffer_size, batch_size)
+        # Seed the random number generator
+        random.seed()
+        # QNetwork - We choose the simple network
+        self.local_net = network.network_simple()
+        self.target_net = network.network_simple()
+
+    # Let the agent learn from experience
+    @abstractmethod
+    def learn(self):
+        pass
+
+    # Take action according to epsilon-greedy-policy:
+    def action(self, state, epsilon=0.01):
+        if random.random() < epsilon:
+            return random.randrange(0, self.action_size)
+        else:
+            prob_distribution = self.local_net.predict(state.reshape(1,-1))
+            action = np.argmax(prob_distribution)
+            return action
+
+    def random_action(self):
+        return random.randrange(0, self.action_size)
+        #return np.random.randint(self.action_size)
+
+    # Copy weights from short-term model to long-term model (soft update)
+    def update_target_net(self, tau=0.1):
+        # Implement soft update for later:
+        # get_weights()[0] -- weights
+        # get weights()[1] -- bias (if existent)
+        # Soft-update:
+        local_weights = np.array( self.local_net.get_weights() )
+        target_weights = np.array( self.target_net.get_weights() )
+        self.target_net.set_weights( tau*local_weights + (1-tau)*target_weights )
+
+    def load_weights(self, path):
+        filepath = os.path.join(path, "ddqn_weights_latest.ckpt")
+        print("Loading network weights from", filepath)
+        self.local_net.load_weights(filepath)
+        self.target_net.load_weights(filepath)
+
+    def save_weights(self, path):
+        filepath = os.path.join(path, "ddqn_weights_latest.ckpt")
+        print("Saving target network weights to", filepath)
+        self.target_net.save_weights(filepath)
+
+
+
+class ReplayBuffer():
+    def __init__(self, buffer_size, batch_size):
+        self.buffer_size = buffer_size
+        self.batch_size = batch_size
+        self.replay_buffer = deque(maxlen=self.buffer_size)
+
+    # Insert experience into memory
+    def insert_into_buffer(self, experience):
+        self.replay_buffer.append(experience)
+
+    # Randomly sample memory
+    def sample_from_buffer(self):
+        # Sample experience batch from experience buffer
+        batch = random.sample(self.replay_buffer, self.batch_size)
+
+        # Reorder experience batch such that we have a batch of states, a batch of actions, a batch of rewards, etc.
+        # Eventually add 'if exp is not None'
+        state = np.vstack( [exp.state for exp in batch] )
+        action = np.vstack( [exp.action for exp in batch] )
+        reward = np.vstack( [exp.reward for exp in batch] )
+        state_next = np.vstack( [exp.next_state for exp in batch] )
+        done = np.vstack( [exp.done for exp in batch] )
+
+        return state, action, reward, state_next, done
+
+    # Get length of memory
+    def buffer_usage(self):
+        return len(self.replay_buffer) > self.batch_size
--- ddqn_agent.py
+++ ddqn_agent.py
@@ -1,11 +1,5 @@
-from collections import deque
-import random
-import os
 import numpy as np
-from collections import namedtuple
-
-import Network
-
+from agent import AbstractAgent
 
 #import keras.backend as K
 
@@ -21,31 +15,20 @@ import Network
 # The last 2 numbers are the left/right turning velocity v_yaw and the forward/backward velocity v_lat of the agent: [v_yaw, v_lat]
 
 
-class DDQNAgent():
+class DDQNAgent(AbstractAgent):
     def __init__(self, buffer_size, batch_size, action_size, gamma):
-        if not batch_size < buffer_size:
-            raise Exception()
-
-        self.buffer_size = buffer_size
-        self.batch_size = batch_size
-        self.action_size = 4
-        self.gamma = gamma
-
-        # Initialize replay buffer
-        self.replay_buffer = ReplayBuffer(buffer_size, batch_size)
-        # Seed the random number generator
-        random.seed()
-        # QNetwork - We choose the simple network
-        self.local_net = Network.network_simple()
-        self.target_net = Network.network_simple()
+        super().__init__(buffer_size, batch_size, action_size, gamma)
 
     # Let the agent learn from experience
     def learn(self):
+        # Check if buffer is sufficiently full:
+        if not self.replay_buffer.buffer_usage():
+            return
+        
         # Retrieve batch of experiences from the replay buffer:
         state_batch, action_batch, reward_batch, next_state_batch, done_batch = self.replay_buffer.sample_from_buffer()
 
-        # Prepare the TD-target. Note that Q_target[:,action] will need to be assigned the 'true' learning target.
-        
+        # Prepare the TD-target
         td_targets = self.local_net.predict( state_batch )
         Q_target = self.target_net.predict( state_batch )
         a_max = np.argmax( self.local_net.predict(next_state_batch), axis=1 )
@@ -62,68 +45,9 @@ class DDQNAgent():
 
         #self.local_net.fit(state_batch, td_targets, batch_size=self.batch_size, epochs=1, shuffle=False, verbose=0)
         self.local_net.train_on_batch(state_batch, td_targets)
-
-    # Take action according to epsilon-greedy-policy:
-    def action(self, state, epsilon=0.99):
-        if random.random() > epsilon:
-            return random.randrange(0, self.action_size)
-        else:
-            prob_distribution = self.local_net.predict(state.reshape(1,-1))
-            action = np.argmax(prob_distribution)
-            return action
-
-    def random_action(self):
-        return random.randrange(0, self.action_size)
-        #return np.random.randint(self.action_size)
-
-    # Copy weights from short-term model to long-term model (soft update)
-    def update_target_net(self, tau=0.1):
-        # Implement soft update for later:
-        # get_weights()[0] -- weights
-        # get weights()[1] -- bias (if existent)
-        # Soft-update:
-        local_weights = np.array( self.local_net.get_weights() )
-        target_weights = np.array( self.target_net.get_weights() )
-        self.target_net.set_weights( tau*local_weights + (1-tau)*target_weights )
-
-    def load_weights(self, path):
-        filepath = os.path.join(path, "ddqn_weights_latest.ckpt")
-        print("Loading network weights from", filepath)
-        self.local_net.load_weights(filepath)
-        self.target_net.load_weights(filepath)
-
-    def save_weights(self, path):
-        filepath = os.path.join(path, "ddqn_weights_latest.ckpt")
-        print("Saving target network weights to", filepath)
-        self.target_net.save_weights(filepath)
+        self.update_target_net()
 
 
 
-class ReplayBuffer():
-    def __init__(self, buffer_size, batch_size):
-        self.buffer_size = buffer_size
-        self.batch_size = batch_size
-        self.replay_buffer = deque(maxlen=self.buffer_size)
 
-    # Insert experience into memory
-    def insert_into_buffer(self, experience):
-        self.replay_buffer.append(experience)
 
-    # Randomly sample memory
-    def sample_from_buffer(self):
-        # Sample experience batch from experience buffer
-        batch = random.sample(self.replay_buffer, self.batch_size)
-
-        # Reorder experience batch such that we have a batch of states, a batch of actions, a batch of rewards, etc.
-        # Eventually add 'if exp is not None'
-        state = np.vstack( [exp.state for exp in batch] )
-        action = np.vstack( [exp.action for exp in batch] )
-        reward = np.vstack( [exp.reward for exp in batch] )
-        state_next = np.vstack( [exp.next_state for exp in batch] )
-        done = np.vstack( [exp.done for exp in batch] )
-
-        return state, action, reward, state_next, done
-
-    # Get length of memory
-    def buffer_usage(self):
-        return len(self.replay_buffer) > self.batch_size
